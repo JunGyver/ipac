104ada4ed9cb9b4f4f053b583ea6a4e8eef5916c
Sangjun <0607juny@gmail.com>
2020-09-07 14:51:19 +0900

    update for aws

--- patent/urls.py
+++ patent/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-from .views import ChartView, HistoryView, MainView
+from .views import (
+    ChartView,
+    HistoryView,
+    MainView,
+    ListView,
+    TestView
+)
 
 urlpatterns = [
     path('chart', ChartView.as_view()),
     path('history', HistoryView.as_view()),
     path('main', MainView.as_view()),
+    path('test', TestView.as_view()),
+    path('list', ListView.as_view()),
 ]
